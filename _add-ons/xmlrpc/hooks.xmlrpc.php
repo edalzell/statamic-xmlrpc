36b4609bf70f65d0843f97e7fc7c372a675aabc8
Erin Dalzell <erin@thedalzells.org>
2014-12-14 21:15:32 -0800

    Supports drafts now

--- _add-ons/xmlrpc/hooks.xmlrpc.php
+++ _add-ons/xmlrpc/hooks.xmlrpc.php
@@ -1,19 +1,25 @@
 <?php
 require_once 'vendor/autoload.php';
 
+use Symfony\Component\Filesystem\Filesystem\Exception\IOException as IOException;
+
 // Utility functions
 function is_set($arr, $field) {
   return (isset($arr[$field]) && (count($arr[$field]) > 0));
 }
 
-// used to hold an entry
-class Entry
+// used to hold an StatamicEntry
+class StatamicEntry
 {
+    const PUBLISH = 'publish';
+    const DRAFT = 'draft';
+    const HIDDEN = 'hidden';
+    
     public $title = '';
     public $categories = '';
     public $tags = '';
     public $content = '';
-    public $post_status = 'publish';
+    public $post_status = StatamicEntry::PUBLISH;
     public $link = null;
     public $author = null;
 }
@@ -21,6 +27,8 @@ class Entry
 class Hooks_xmlrpc extends Hooks
 {
     private $link_custom_field;
+    private $author_custom_field;
+    
     private $current_user;
     
     // https://github.com/danielberlinger/rsd
@@ -50,43 +58,36 @@ RSD;
 
     public function xmlrpc__api()
     {
-        global $link_custom_field, $author_custom_field;
-        
-        $link_custom_field = $this->fetchConfig('link_custom_field', 'titlelink');
-        $author_custom_field = $this->fetchConfig('author_custom_field', 'author');
+        $this->link_custom_field = $this->fetchConfig('link_custom_field', 'titlelink');
+        $this->author_custom_field = $this->fetchConfig('author_custom_field', 'author');
 
-        try {
-            // register the callback and process the POST request
-            $this->server = new IXR_Server(array(
-                'metaWeblog.getPost' => array($this, 'getPost'),
-                'metaWeblog.newPost'  => array($this, 'newPost'),
-                'metaWeblog.editPost' => array($this, 'editPost'),
-                'metaWeblog.deletePost' => array($this, 'deletePost'),
-                'metaWeblog.getCategories' => array($this, 'getCategories'),
-                'metaWeblog.getRecentPosts' => array($this, 'getRecentPosts'),
-                'metaWeblog.getUsersBlogs' => array($this, 'getUsersBlogs' ),
-                'metaWeblog.newMediaObject' => array($this, 'newMediaObject' ),
+        $this->server = new IXR_Server(array(
+            'metaWeblog.getPost' => array($this, 'getPost'),
+            'metaWeblog.newPost'  => array($this, 'newPost'),
+            'metaWeblog.editPost' => array($this, 'editPost'),
+            'metaWeblog.deletePost' => array($this, 'deletePost'),
+            'metaWeblog.getCategories' => array($this, 'getCategories'),
+            'metaWeblog.getRecentPosts' => array($this, 'getRecentPosts'),
+            'metaWeblog.getUsersBlogs' => array($this, 'getUsersBlogs' ),
+            'metaWeblog.newMediaObject' => array($this, 'newMediaObject' ),
 
-                // Blogger
-                'blogger.getUsersBlogs' => array($this, 'getUsersBlogs'),
-                'blogger.deletePost' => array($this, 'deletePost'),
+            // Blogger
+            'blogger.getUsersBlogs' => array($this, 'getUsersBlogs'),
+            'blogger.deletePost' => array($this, 'deletePost'),
 
-                // MoveableType
-                'mt.supportedTextFilters' => array($this, 'supportedTextFilters'),
-                'mt.getPostCategories' => array($this, 'getPostCategories'),
-                'mt.setPostCategories' => array($this, 'setPostCategories'),
-                'mt.getCategoryList' => array($this, 'getCategories'),
-        
-                // Wordpress
-                'wp.getUsersBlogs' => array($this, 'getUsersBlogs'),
-            ));
-        } catch (AuthorizationException $e) {
-            return new IXR_Error(403, "Incorrect username or password");
-        }
+            // MoveableType
+            'mt.supportedTextFilters' => array($this, 'supportedTextFilters'),
+            'mt.getPostCategories' => array($this, 'getPostCategories'),
+            'mt.setPostCategories' => array($this, 'setPostCategories'),
+            'mt.getCategoryList' => array($this, 'getCategories'),
+            'mt.publishPost' => array($this, 'publishPost'),
+    
+            // Wordpress
+            'wp.getUsersBlogs' => array($this, 'getUsersBlogs'),
+        ));
     }
     
     private function authenticate($username, $password) {
-        global $current_user;
         
         // attempt to load the Member object
         $user = Auth::getMember($username);
@@ -100,16 +101,10 @@ RSD;
             $app->halt(200, $error->getXml());
         }
         
-        $current_user = $username;
+        $this->current_user = $username;
     }
     
-    private function getUsername() {
-        global $current_user;
-        
-        return ($current_user);
-    }
-    
-    // from a page and an entry slug, create the full path
+    // from a page and an StatamicEntry slug, create the full path
     private function getFullPath($folder, $slug) {
         return Path::assemble(BASE_PATH, Config::getContentRoot(), Path::resolve($folder . '/' . $slug) . '.' . Config::getContentType());
     }
@@ -122,22 +117,62 @@ RSD;
         return explode('#', $postid);
     }
     
-    private function getEntryStatus($entry) {
-        $status = 'publish';
+    /* statamic only has 3 statuses: publish, draft, hidden, but we might receive
+       anyone of these statuses: http://codex.wordpress.org/Post_Status_Transitions
+       so we have to map appropriately:
+       
+       new, publish, inherit, trash -> publish
+       pending, draft, auto-draft, future -> draft
+       private -> hidden
+       
+       HOWEVER, if the publish flag is false it must be a draft, regardless of the
+       post status.
+    */
+    private function convertPostStatusToEntryStatus($post_status, $publish) {
+        $status = '';
+        
+        $draft = array('pending', 'draft', 'auto-draft', 'future');
+        $hidden = array('private');
+        
+        if (!$publish || in_array($post_status, $draft)) {
+            $status = StatamicEntry::DRAFT;
+        } else if (in_array($post_status, $hidden)) {
+            $status = StatamicEntry::HIDDEN;
+        } else {
+            $status = StatamicEntry::PUBLISH;
+        }
+        
+        return $status;
+    }
+    
+    private function convertEntryStatusToPostStatus($entry) {
+        $status = StatamicEntry::PUBLISH;
         
         if (isset($entry['_is_draft']) && ($entry['_is_draft'] == 1)) {
-            $status = 'draft';
+            $status = StatamicEntry::DRAFT;
+        } else if (isset($entry['_is_hidden']) && ($entry['_is_hidden'] == 1)) {
+            $status = StatamicEntry::HIDDEN;
         }
     
         return $status;
     }
     
+    private function statusesAreEqual($entry, $content) {
+        $isHidden = $content['_is_hidden'];
+        $isDraft = $content['_is_draft'];
+        $isLive = !$isHidden && !$isDraft;
+        
+        return (($isDraft && ($entry->post_status === StatamicEntry::DRAFT)) ||
+                ($isHidden && ($entry->post_status === StatamicEntry::HIDDEN)) ||
+                ($isLive && ($entry->post_status === StatamicEntry::PUBLISH)));
+    }
+    
     // create the structure required for the MetaWeblog API
     private function convertEntryToPost($entry) {
-        global $link_custom_field, $author_custom_field;
         
+        // if no author, use the currently authenticated user
         if (!isset($entry['author']) || empty($entry['author'])) {
-            $entry['author'] = $this->getUsername();
+            $entry['author'] = $this->current_user;
         } 
         
         $post = array(
@@ -148,13 +183,13 @@ RSD;
             'permaLink' => $entry['permalink'],
             'categories' => isset($entry['categories']) ? $entry['categories'] : null,
             'dateCreated' => date(DateTime::ISO8601, $entry['datestamp']),
-            'post_status' => $this->getEntryStatus($entry),
+            'post_status' => $this->convertEntryStatusToPostStatus($entry),
             'custom_fields' => array(
                 array(
-                    'key' => $link_custom_field,
+                    'key' => $this->link_custom_field,
                     'value' => isset($entry['link']) ? $entry['link'] : null ),
                 array(
-                    'key' => $author_custom_field,
+                    'key' => $this->author_custom_field,
                     'value' => $entry['author'] ),
                 ),  
         );
@@ -162,12 +197,10 @@ RSD;
         return $post;
     }
     
-    private function convertPostToEntry($post) {
-        global $link_custom_field, $author_custom_field;
-        
-        $entry = new Entry();
+    private function convertPostToEntry($post, $publish=true) {
+        $entry = new StatamicEntry();
 
-        // get the entry data
+        // get the StatamicEntry data
         $entry->title = $post['title'];
         
         if (isset($post['description'])) {
@@ -182,34 +215,30 @@ RSD;
             $entry->tags = $post['tags'];
         }
         
-        if (isset($post['post_status'])) {
-            $entry->post_status = $post['post_status'];
-        }
+        $entry->post_status = $this->convertPostStatusToEntryStatus($post['post_status'], $publish);
         
         if (is_set($post, 'custom_fields')) {
             foreach ($post['custom_fields'] as $field) {
-                if ($field['key'] === $link_custom_field) {
+                if ($field['key'] === $this->link_custom_field) {
                     $entry->link = $field['value'];
                     break;
                 }
-                if ($field['key'] === $author_custom_field) {
+                if ($field['key'] === $this->author_custom_field) {
                     $entry->author = $field['value'] ?: $this->getUsername();
                     break;
                 }
             }
         // if there are no custom fields, we still need to set the author
         } else {
-            $entry->author = $this->getUsername();
+            $entry->author = $this->current_user;
         }
         return $entry;
     }
     
     private function convertContentToEntry($content) {
-        global $link_custom_field, $author_custom_field;
-        
-        $entry = new Entry();
+        $entry = new StatamicEntry();
 
-        // get the entry data
+        // get the StatamicEntry data
         $entry->title = $content['title'];
         
         if (isset($content['content_raw'])) {
@@ -232,11 +261,28 @@ RSD;
             $entry->author = $content['author'];
         // if there are no custom fields, we still need to set the author
         } else {
-            $entry->author = $this->getUsername();
+            $entry->author = $this->current_user;
         }
         return $entry;
     }
 
+    function changeStatus($page, $slug, $status) {
+        $originalFilePath = $this->getFullPath($page, $slug);
+
+        $pathParts = explode('/', $originalFilePath);
+        
+        // remove the old prefix then append the new one.
+        
+        //strip the prefix
+        $filename = preg_replace('/_+/', '', end($pathParts));
+        
+        $status_prefix = Slug::getStatusPrefix($status);
+       
+        $pathParts[count($pathParts) - 1] = $status_prefix . $filename;
+
+        File::move($originalFilePath, implode('/', $pathParts));
+    }
+    
     private function saveEntryToFile($entry, $path) {
     
         // Front matter
@@ -250,8 +296,7 @@ RSD;
         if (isset($entry->link) && !empty($entry->link)) {
             $data['link'] = $entry->link;
         }
-
-        // write the file
+        
         File::put($path, File::buildContent($data, $entry->content));
     }
     
@@ -266,7 +311,7 @@ RSD;
         // grab the recent posts
         $content_set = ContentService::getContentByFolders($blog);
 
-        // have to filter out everything that's NOT an entry
+        // have to filter out everything that's NOT an StatamicEntry
         $content_set->filter(array( 'type' => 'entries' ));
 
         // sort entries newest to oldest
@@ -278,7 +323,7 @@ RSD;
         //get content
         $content_array = $content_set->get(true, false);
 
-        // loop through each entry and convert to a post
+        // loop through each StatamicEntry and convert to a post
         foreach ($content_array as $entry) {
             $posts[] = $this->convertEntryToPost($entry);   
         }
@@ -292,10 +337,10 @@ RSD;
 
         $this->authenticate($username, $password);
         
-        // parse out the page & the entry id
+        // parse out the page & the StatamicEntry id
         list($page, $slug) = $this->parsePostId($postid);
         
-        //convert entry to MetaWeblog post
+        //convert StatamicEntry to MetaWeblog post
         return $this->convertEntryToPost(Content::get(URL::assemble($page, $slug)));
     }
     
@@ -304,7 +349,7 @@ RSD;
     function newPost($params) {
         // pull data out of POST params
         // page is folder to save to
-        list($page, $username, $password, $post) = $params;
+        list($page, $username, $password, $post, $publish) = $params;
         
         $this->authenticate($username, $password);
         
@@ -314,32 +359,33 @@ RSD;
         // create the appropriate prefix
         $entry_type = Statamic::get_entry_type($page_path);
 
-        $prefix = "";
+        $order_prefix = "";
         if ($entry_type == 'date') {
             if (Config::get('_entry_timestamps')) {
-                $prefix = date('Y-m-d-Hi-');
+                $order_prefix = date('Y-m-d-Hi-');
             }
             else {
-                $prefix = date('Y-m-d-');
+                $order_prefix = date('Y-m-d-');
             }
         } else if ($entry_type == 'number') {
-            $prefix = Statamic::get_next_numeric($page_path) . "-";
+            $order_prefix = Statamic::get_next_numeric($page_path) . "-";
         }
 
-        // get the entry data
-        $entry = $this->convertPostToEntry($post);
+        // get the Entry data
+        $entry = $this->convertPostToEntry($post, $publish);
 
         // the slug comes from the title in lowercase with '-' as a delimiter
         $slug = Slug::make($entry->title, array('lowercase' => true));
 
+        // get the status prefix from the post status
+        $status_prefix = Slug::getStatusPrefix($entry->post_status);
+
         // make the file name
-        $filename = $prefix . $slug;
+        $filename = $status_prefix . $order_prefix . $slug;
 
         $fullpath = $this->getFullPath($page_path, $filename);
         
-        $prefix = $entry->post_status == 'draft' ? "__" : "";
-        
-        $this->saveEntryToFile($entry, $prefix . $fullpath);
+        $this->saveEntryToFile($entry, $fullpath);
         
         // this should be unique so clients can use this as the key.
         return $this->makePostId($page, $slug);
@@ -355,7 +401,7 @@ RSD;
         list($page, $slug) = $this->parsePostId($postid);
 
         /*
-            To properly set the categories in Moveable Type:
+            To properly set the categories in Moveable Type from MarsEdit:
                 1) metaWeblog.editPost - no categories are passed & publish is false
                 2) mt.setPostCategories - to set the categories 
                 3) metaWeblog.editPost - no categories are passed & publish is true
@@ -369,7 +415,7 @@ RSD;
 
             So, what we nee do to is check to see if we are doing MT by checking to see
             if the categories are blank. If so, then when we receive the 'true' publish
-            parameter, retain the categories that are in the file alread
+            parameter, retain the categories that are in the file already
         */
 
         $content = Content::get(URL::assemble($page, $slug));
@@ -380,12 +426,28 @@ RSD;
             $post['categories'] = $content['categories'];
         }
         
-        // convert to a Statamic entry
+        // convert to a Statamic Entry
         $entry = $this->convertPostToEntry($post);
 
         $this->saveEntryToFile($entry, $content['_file']);
-
+     
+        if (!$this->statusesAreEqual($entry, $content)) {
+            $this->changeStatus($page, $slug, $entry->post_status);
+        }
+    
         return true;
+    }
+    
+    // http://codex.wordpress.org/XML-RPC_MovableType_API#mt.publishPost
+    function publishPost($params) {
+        list($postid, $username, $password) = $params;
+
+        $this->authenticate($username, $password);
+        
+        list($page, $slug) = $this->parsePostId($postid);
+        
+        $this->changeStatus($page, $slug, StatamicEntry::PUBLISH);
+       
     }
     
     // http://codex.wordpress.org/XML-RPC_MetaWeblog_API#metaWeblog.deletePost
@@ -394,15 +456,21 @@ RSD;
 
         $this->authenticate($username, $password);
         
-        // grab the page & the entry id
+        // grab the page & the StatamicEntry id
         list($page, $slug) = $this->parsePostId($postid);
         
         $path = $this->getFullPath($page, $slug);
         
-        File::delete($path);
-        // TODO: figure out how to throw an error back
-        //$app = \Slim\Slim::getInstance();
-        //$app->halt(401, 'Uh oh!');
+        try {
+            File::delete($path);
+        } catch (IOException $ioe)
+        {
+            $error = new IXR_Error(403, $ioe->message);
+            
+            // halt immediately stop and send back a response
+            $app = \Slim\Slim::getInstance();
+            $app->halt(200, $error->getXml());
+        }
         
         return true;
     }
@@ -530,10 +598,6 @@ RSD;
         return($imgdata);
     }
     
-    function supportedMethods($params) {
-        return $functions;
-    }
-
     function supportedTextFilters($params) {
         return array();
     }
